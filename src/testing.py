4eecba6d1d8cfbad08cf50ae2ec19a54b612ad8a
drifter1 <drifterkosmas@gmail.com>
2021-09-07 16:15:07 +0300

    Testing Script creates Transaction from UTXO The testing script now contacts the full node in order to retrieve its corresponding utxo. From that utxo it chooses the first utxo output and retrieves the block transaction output it references (for the value and hash fields). Afterwards, it creates a small transaction where it sends the total value to another "testing" address. So, the mining script is now needed in order for the testing script to be able to find a UTXO entry to create the transaction from. In order to test the system out now, all four scripts are needed (dns_server, full_node, miner, testing).

--- src/testing.py
+++ src/testing.py
@@ -4,13 +4,15 @@ import json
 from argparse import ArgumentParser
 
 from full_node.settings import Full_Node_Settings
-from common.node_requests import general_retrieve_nodes
-from common.block import Block, calculate_block_hash, json_construct_block
-from common.block_requests import general_create_block
-from common.transaction import Input, Output, Transaction, calculate_output_hash, calculate_transaction_hash, json_construct_transaction, sign_input
-from common.transaction_requests import general_post_transaction
-from common.utxo_requests import general_retrieve_utxo_address
+
+from common.transaction import Input, Output, Transaction, calculate_output_hash, calculate_transaction_hash, json_construct_transaction, json_destruct_output, sign_input
+from common.utxo import json_destruct_utxo_output
 from common.wallet import Wallet, json_construct_wallet, json_retrieve_address, json_retrieve_private_key
+
+from common.block_requests import general_retrieve_block_transaction_output
+from common.transaction_requests import general_post_transaction
+from common.node_requests import general_retrieve_nodes
+from common.utxo_requests import general_retrieve_utxo_address
 
 # Setup Files Routine
 
@@ -93,37 +95,6 @@ private_key = json_retrieve_private_key(wallet_json)
 
 print(address)
 
-# create reward transaction
-
-reward_input = Input(
-    output_value=2.5
-)
-
-reward_output = Output(
-    address=address,
-    value=2.5
-)
-calculate_output_hash(reward_output)
-
-reward_transaction = Transaction(
-    inputs=[reward_input],
-    outputs=[reward_output],
-    value=2.5,
-    fee=0
-)
-calculate_transaction_hash(reward_transaction)
-
-# create block 0
-
-block0 = Block(height=0, creator=address, reward=2.5, fees=0, nonce="abcdef",
-               transaction_count=1, transactions=[reward_transaction])
-
-calculate_block_hash(block0)
-
-json_block0 = json_construct_block(block0)
-
-general_create_block(settings, json_node, json_block0)
-
 # retrieve utxo for the wallet address
 
 time.sleep(1)
@@ -131,46 +102,51 @@ time.sleep(1)
 json_utxo, status_code = general_retrieve_utxo_address(
     settings, json_node, address)
 
+if status_code != 200:
+    exit()
+
 print(json_utxo)
 
-time.sleep(10)
+# retrieve first UTXO output
+try:
+    json_utxo_output = json_utxo["utxo_outputs"][0]
+    utxo_output = json_destruct_utxo_output(json_utxo_output)
+except:
+    exit()
+
+# retrieve corresponding transaction output in block
+json_block_transaction_output, status_code = general_retrieve_block_transaction_output(
+    settings, json_node, utxo_output.block_height, utxo_output.transaction_index, utxo_output.output_index)
+
+if status_code != 200:
+    exit()
+
+print(json_block_transaction_output)
+
+block_transaction_output = json_destruct_output(json_block_transaction_output)
 
 # create and post test transaction
 
 input0 = Input(
-    transaction_hash=reward_transaction.hash,
+    transaction_hash=utxo_output.transaction_hash,
     output_index=0,
     output_address=address,
-    output_value=reward_output.value,
-    output_hash=reward_output.hash,
+    output_value=block_transaction_output.value,
+    output_hash=block_transaction_output.hash,
 )
 sign_input(input0, private_key)
 
 output0 = Output(
     index=0,
     address="0x60a192daca0804e113d6e6d41852c611be5de0bf",
-    value=1.2
+    value=input0.output_value - 0.001
 )
 calculate_output_hash(output0)
 
-output1 = Output(
-    index=1,
-    address="0xe23fc383c7007002ef08be610cef95a86ce44e26",
-    value=0.3
-)
-calculate_output_hash(output1)
-
-output2 = Output(
-    index=2,
-    address=address,
-    value=0.999
-)
-calculate_output_hash(output2)
-
 transaction = Transaction(
     inputs=[input0],
-    outputs=[output0, output1, output2],
-    value=2.499,
+    outputs=[output0],
+    value=output0.value,
     fee=0.001
 )
 calculate_transaction_hash(transaction)
@@ -178,47 +154,3 @@ calculate_transaction_hash(transaction)
 json_transaction = json_construct_transaction(transaction)
 
 general_post_transaction(settings, json_node, json_transaction)
-
-time.sleep(10)
-
-# create reward transaction 2
-
-reward_input2 = Input(
-    output_value=2.5
-)
-
-reward_output2 = Output(
-    address=address,
-    value=2.5
-)
-calculate_output_hash(reward_output2)
-
-reward_transaction2 = Transaction(
-    inputs=[reward_input2],
-    outputs=[reward_output2],
-    value=2.5,
-    fee=0
-)
-
-calculate_transaction_hash(reward_transaction2)
-
-# create block 1
-
-block1 = Block(height=1, creator=address, reward=2.5, fees=0.001,
-               nonce="123456", prev_hash=block0.hash, transaction_count=2,
-               transactions=[reward_transaction2, transaction])
-
-calculate_block_hash(block1)
-
-json_block1 = json_construct_block(block1)
-
-general_create_block(settings, json_node, json_block1)
-
-# retrieve utxo for the wallet address
-
-time.sleep(1)
-
-json_utxo, status_code = general_retrieve_utxo_address(
-    settings, json_node, address)
-
-print(json_utxo)
